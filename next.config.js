9a8704a7a8ff6e7463e5199b0b63278fb957a718
Gaku Ishikawa <gaku.jp.301@gmail.com>
2024-01-04 21:52:31 +0900

    fix blog page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['blog.gakux.net', 'apple-resources.s3.amazonaws.com', 'play.google.com']
+    domains: ['zenn.dev', 'apple-resources.s3.amazonaws.com', 'play.google.com']
   }
 }
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,39 +1,27 @@
 import Link from "next/link";
-import Image from "next/image";
 import Layout from "../layout";
 import Footer from "../../components/footer";
 import Header from "../../components/header";
-import axios from "axios";
-import { UrlLinks } from "../../utils/const";
 
 
 export const getStaticProps = async () => {
-  const url = "https://blog.gakux.net/wp-json/wp/v2/posts?_embed";
-  let data = {
-    categories: 6, //sake
-    per_page: 5,// 記事数
-  }
+  const url = "https://zenn.dev/api/articles?username=takanexx&order=latest";
   try {
-    // Sakeカテゴリの記事を取得
-    const sake_res = await axios.get(url, {data:data});
-    let sakePosts = sake_res.data;
-    // Techカテゴリの記事を取得
-    data['categories'] = 5;
-    const tech_res = await axios.get(url,{data:data});
-    let techPosts = tech_res.data;
+    // Zennから記事を取得
+    const res = await fetch(url);
+    const data = await res.json();
     return {
-      props: { sakePosts, techPosts }
+      props: {posts: data.articles ?? []}
     }
   } catch (error) {
-    let sakePosts = [];
-    let techPosts = [];
+    const posts = [];
     return {
-      props: { sakePosts, techPosts }
+      props: {posts}
     }
   }
 }
 
-const Index = ({ sakePosts, techPosts }) => {
+const Index = ({ posts }) => {
   const title = "blog";
   const description = "This is blog page. Check it out.";
   const keywords = "blog,article,posts,ブログ,記事";
@@ -63,75 +51,25 @@ const Index = ({ sakePosts, techPosts }) => {
             <h1 className="text-2xl font-extrabold">blog</h1>
           </div>
           <div className="justify-start pb-10">
-            <div className="py-5 flex justify-center items-center">
-              <hr className="h-1/2 w-14 border-black"/>
-              <h2 className="text-3xl font-extralight mx-5">Tech</h2>
-              <hr className="h-1/2 w-14 border-black"/>
-            </div>
             <ul>
-              {techPosts?.map((post) => (
+              {posts?.map((post) => (
                 <li 
                   className="group relative mb-5 outline outline-1 outline-offset-2 outline-gray-200 rounded-sm" 
                   key={post.id}
                 >
-                  <Link href={post.link}>
+                  <Link href={`https://zenn.dev/${post.path}`}>
                     <a 
-                      className="xs:flex-none xs:justify-center md:flex md:justify-start transition grayscale hover:scale-110 hover:grayscale-0"
+                      className="xs:flex-none xs:justify-center md:flex md:justify-start transition hover:scale-110"
                       target="_blank" 
                       rel="noreferrer noopener"
                     >
-                      <div className="xs:w-full md:w-1/3 flex items-center">
-                        <Image 
-                          src={post._embedded['wp:featuredmedia'][0].source_url}
-                          width={320} 
-                          height={180} 
-                          layout="fixed"
-                          alt="image"
-                        />
+                      <div className="xs:w-full md:w-1/3 flex items-center justify-center">
+                        <div className="text-7xl">{post.emoji}</div>
                       </div>
                       <div className="p-5 xs:w-full md:w-2/3">
-                        <h3 className="text-3xl font-bold xs:pb-4">{post.title.rendered}</h3>
+                        <h3 className="text-3xl font-bold xs:pb-4">{post.title}</h3>
                         <div className="w-fit mt-5 xs:mt-5">
-                          <p className="font-thin">{getDate(post.date)}</p>
-                        </div>
-                      </div>
-                    </a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="justify-start pb-10">
-            <div className="py-5 flex justify-center items-center">
-              <hr className="h-1/2 w-14 border-black"/>
-              <h2 className="text-3xl font-extralight mx-5">Sake</h2>
-              <hr className="h-1/2 w-14 border-black"/>
-            </div>
-            <ul>
-              {sakePosts?.map((post) => (
-                <li 
-                  className="group relative mb-5 outline outline-1 outline-offset-2 outline-gray-200 rounded-sm" 
-                  key={post.id}
-                >
-                  <Link href={post.link}>
-                    <a 
-                      className="xs:flex-none xs:justify-center md:flex md:justify-start transition grayscale hover:scale-110 hover:grayscale-0"
-                      target="_blank" 
-                      rel="noreferrer noopener"
-                    >
-                      <div className="xs:w-full md:w-1/3 flex items-center">
-                        <Image 
-                          src={post._embedded['wp:featuredmedia'][0].source_url}
-                          width={320} 
-                          height={180} 
-                          layout="fixed"
-                          alt="image"
-                        />
-                      </div>
-                      <div className="p-5 xs:w-full md:w-2/3">
-                        <h3 className="text-3xl font-bold xs:pb-4">{post.title.rendered}</h3>
-                        <div className="w-fit mt-5 xs:mt-5">
-                          <p className="font-thin">{getDate(post.date)}</p>
+                          <p className="font-thin">{getDate(post.published_at)}</p>
                         </div>
                       </div>
                     </a>
@@ -141,7 +79,7 @@ const Index = ({ sakePosts, techPosts }) => {
             </ul>
           </div>
           <div className="flex justify-center mx-auto pt-3">
-            <Link href={UrlLinks.blogLink}>
+            <Link href={`https://zenn.dev/takanexx`}>
               <a target="_blank" rel="noreferrer noopener">read more &rarr;</a>
             </Link>
           </div>
